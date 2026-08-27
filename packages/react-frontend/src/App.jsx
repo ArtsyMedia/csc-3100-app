c3febe302fd27cd5a07a0717b083ea9f5554d942
Max Plankar <maxplankar@gmail.com>
2026-08-27 11:53:46 -0700

    updated text

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <div>
           <h1>Get started</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            Hi my name is max and this is some new text!
           </p>
         </div>
         <button
