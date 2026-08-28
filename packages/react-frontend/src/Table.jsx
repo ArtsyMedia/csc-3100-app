1c11239bf80d226f3065379ee43ad6934389d20c
Max Plankar <maxplankar@gmail.com>
2026-08-28 16:48:57 -0700

    completed 4-6

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -1,44 +1,45 @@
 // src/Table.jsx
 function TableHeader() {
-  return (
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Job</th>
-      </tr>
-    </thead>
-  );
-}
-
-function TableBody() {
-  return (
-    <tbody>
-      <tr>
-        <td>Charlie</td>
-        <td>Janitor</td>
-      </tr>
-      <tr>
-        <td>Mac</td>
-        <td>Bouncer</td>
-      </tr>
-      <tr>
-        <td>Dee</td>
-        <td>Aspiring actress</td>
-      </tr>
-      <tr>
-        <td>Dennis</td>
-        <td>Bartender</td>
-      </tr>
-    </tbody>
-  );
-}
-
-function Table() {
     return (
-      <table>
-        <TableHeader />
-        <TableBody />
-      </table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Remove</th>
+            </tr>
+        </thead>
+    );
+}
+
+function TableBody(props) {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+                <td>
+                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+                </td>
+            </tr>
+        );
+    }
+    );
+    return (
+        <tbody>
+            {rows}
+        </tbody>
+    );
+}
+
+function Table(props) {
+    return (
+        <table>
+            <TableHeader />
+            <TableBody
+                characterData={props.characterData}
+                removeCharacter={props.removeCharacter}
+            />
+        </table>
     );
 }
 
