1cb669c91d991273a2c1f896f18bd09ad4791ca8
Max Plankar <maxplankar@gmail.com>
2026-09-09 13:43:54 -0700

    better search capabilities for job

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -36,17 +36,22 @@ const users = {
     ],
 };
 
-app.get("/users", (req, res) => {
-  res.send(users);
-});
-
 const findUserByName = (name) => {
     return users["users_list"].filter((user) => user["name"] === name);
 };
 
+const findUsersByNameAndJob = (name, job) => {
+    return users["users_list"].filter(
+        (user) => user["name"] === name && user["job"] === job
+    );
+};
+
 app.get("/users", (req, res) => {
     const name = req.query.name;
-    if (name != undefined) {
+    const job = req.query.job;
+    if (name != undefined && job != undefined) {
+        res.send({ users_list: findUsersByNameAndJob(name, job) });
+    } else if (name != undefined) {
         let result = findUserByName(name);
         result = { users_list: result };
         res.send(result);
@@ -54,6 +59,8 @@ app.get("/users", (req, res) => {
         res.send(users);
     }
 });
+
+
 
 
 const findUserById = (id) =>
